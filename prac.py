affd20c17b3bd90e94297120a64f90a6d7bae646
Benjamin Hodges <ben@TammyRole.local>
2026-05-22 09:53:53 -0500

    Ignore macOS .DS_Store files

--- prac.py
+++ prac.py
@@ -188,3 +188,17 @@ has_insurance = input() == "true"
 
 result = age >= 18 and has_insurance and has_license 
 print(result)"""
+
+"""wind = int(input()) # Don't change this line
+status = "unset"
+# Type your code below
+if wind < 8:
+    status = "Calm"
+elif wind >= 8  and wind <= 31:
+    status = "Breeze"
+elif wind >= 32 and wind <= 63:
+    status = "Gale"
+else: status = "storm"
+# Don't change the line below
+print(f"status = {status}").    """
+
